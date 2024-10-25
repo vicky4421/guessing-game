7a2fa1d9493bdcda8839ca3cd7db2f0bc3eeb882
Vivek Sawant <vicky210490@gmail.com>
2024-10-25 15:58:05 +0530

    write test

--- tests/guessing-game.ts
+++ tests/guessing-game.ts
@@ -1,16 +1,35 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { GuessingGame } from "../target/types/guessing_game";
+import { networkStateAccountAddress, Orao, randomnessAccountAddress } from "@orao-network/solana-vrf";
 
 describe("guessing-game", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
 
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.GuessingGame as Program<GuessingGame>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  let force_seed = anchor.web3.Keypair.generate().publicKey.toBuffer();
+
+  const vrf = new Orao(provider);
+  const random = randomnessAccountAddress(force_seed);
+  const treasury = new anchor.web3.PublicKey("7Wa5jXzh5SWPL8AnDM3fkPpSc391nys7zWzyNnabGXH");
+
+  it("Got a random number from Orao VRF program", async () => {
+    console.log("is this working?");
+    const tx = await program.methods
+      .init([...force_seed])
+      .accounts({
+        payer: provider.wallet.publicKey,
+        treasury,
+        oraVrf: vrf.programId,
+        random,
+        networkState: networkStateAccountAddress(),
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc({ skipPreflight: true });
+
+    await vrf.waitFulfilled(force_seed);
   });
 });
